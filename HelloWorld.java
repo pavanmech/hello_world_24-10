9435417d5cf8bfbbbd92cac42bb44f398bc1a843
pavanmech <153759227+pavanmech@users.noreply.github.com>
2024-10-25 23:31:50 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
     System.out.println("Hello world");
     System.out.println("Hello world");
     System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
   }
 }
 
